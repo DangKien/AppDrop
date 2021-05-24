3b9be289640f88a7cde61c735027ca1986d6d179
kiendt <dangtrungkien96@gmail.com>
2021-05-24 16:26:29 +0700

    product detail

--- src/scripts/product.js
+++ src/scripts/product.js
@@ -1,2 +1,20 @@
+/** @format */
+
 import "../styles/home.scss";
 import "../styles/product.scss";
+
+import Swiper from "swiper";
+// import Swiper styles
+import "swiper/swiper-bundle.css";
+
+document.onreadystatechange = () => {
+  if (document.readyState === "complete") {
+    new Swiper(".slider-product", {
+      loop: true,
+      slidesPerView: 1,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+  }
+};
